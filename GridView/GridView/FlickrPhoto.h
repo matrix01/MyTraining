ed053a7a4463f5e448d932ff581c78ba9b36714f
matrix01 <milon06@gmail.com>
2015-12-02 21:34:17 +0600

    GridView Project

---
+++ GridView/GridView/FlickrPhoto.h
@@ -0,0 +1,15 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface FlickrPhoto : NSObject
+@property(nonatomic,strong) UIImage *thumbnail;
+@property(nonatomic,strong) UIImage *largeImage;
+
+// Lookup info
+@property(nonatomic) long long photoID;
+@property(nonatomic) NSInteger farm;
+@property(nonatomic) NSInteger server;
+@property(nonatomic,strong) NSString *secret;
+
+@end
---
+++ GridView/GridView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GridView
+//
+//  Created by Milan Mia on 12/2/15.
+//  Copyright © 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
